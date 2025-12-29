64bb89db9025b56c367e82551aa822e550002828
Rafael Marinho <c0645326@vale.com>
2025-12-28 23:21:34 -0300

    feat: create rating value object

---
+++ backend/MarinhoDiscos.Domain/ValueObjects/Rating.cs
@@ -0,0 +1,20 @@
+﻿namespace MarinhoDiscos.Domain.ValueObjects;
+
+public sealed class Rating
+{
+    public int Value { get; }
+
+    public Rating(int value)
+    {
+        if (value < 0 || value > 10)
+            throw new DomainException("Rating must be between 0 and 10");
+
+        Value = value;
+    }
+
+    public override bool Equals(object? obj)
+        => obj is Rating other && Value == other.Value;
+
+    public override int GetHashCode()
+        => Value.GetHashCode();
+}
