d4f8f254e6910011a45d90a9f5223e806741edd3
Rafael Marinho <c0645326@vale.com>
2026-02-01 23:33:18 -0300

    feat: add album repository

---
+++ backend/MarinhoDiscos.Infrastructure/Repositories/AlbumRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MarinhoDiscos.Domain.Repositories;
+using MarinhoDiscos.Domain.Entities;
+using MarinhoDiscos.Infrastructure.Persistence;
+
+namespace MarinhoDiscos.Infrastructure.Repositories;
+
+public class AlbumRepository : IAlbumRepository
+{
+    private readonly MarinhoDiscosDbContext _context;
+
+    public AlbumRepository(MarinhoDiscosDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Album?> GetByIdAsync(Guid id)
+    {
+        return await _context.Set<Album>().FindAsync(id);
+    }
+
+    public async Task AddAsync(Album album)
+    {
+        await _context.Set<Album>().AddAsync(album);
+    }
+}
